d9350bdab4b9acebbd273365418ea75051e18114
UH5555 <zhuhao162@163.com>
2022-03-28 20:31:53 +0800

    0328\指针详解（5）

--- test_5.c
+++ test_5.c
@@ -140,14 +140,140 @@
 //	return 0;
 //}
 
-char* my_strcpy(char* dest, const char* src)
-//写一个函数指针pf，能够指向my_strcpy
-//写一个函数指针数组pfArr，能够存放4个my_strcpy函数的地址
+//char* my_strcpy(char* dest, const char* src);
+////写一个函数指针pf，能够指向my_strcpy
+////写一个函数指针数组pfArr，能够存放4个my_strcpy函数的地址
+//
+//int main()
+//{
+//	char*(*pf)(char*, const char*) = my_strcpy;
+//	char* (*ppArr[4])(char*, const char*) = { my_strcpy };
+//}
+
+//void menu()
+//{
+//	printf("******************\n");
+//	printf("***1.Add  2.Sub***\n");
+//	printf("***3.Mul  4.Div***\n");
+//	printf("***** 0.exit *****\n");
+//}
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//
+////int main()
+////{
+////	int input = 0;
+////	int x = 0;
+////	int y = 0;
+////	//pfArr - 是一个函数指针数组  - 转移表
+////	int (*pfArr[5])(int, int)={0,Add,Sub,Mul,Div};
+////	do
+////	{
+////		menu();
+////		printf("请选择:");
+////		scanf("%d", &input);
+////		if (input >= 1 && input <= 4)
+////		{
+////			printf("请输入两个操作数：");
+////			scanf("%d%d", &x, &y);
+////			printf("%d\n", pfArr[input](x, y));
+////		}
+////		else if (input == 0)
+////		{
+////			printf("退出\n");
+////		}
+////		else
+////			printf("选择错误！\n");
+////	} while (input);
+////	return 0;
+////}
+//
+//void Calc(int(*pf)(int, int))
+//{
+//	int x = 0;
+//	int y = 0;
+//	printf("请输入两个操作数：");
+//	scanf("%d%d", &x, &y);
+//	printf("%d\n", pf(x, y));
+//}
+//
+//int main()
+//{
+//	int input = 0;
+//	do
+//	{
+//		menu();
+//		printf("请选择：");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			Calc(Add);
+//			break;
+//		case 2:
+//			Calc(Sub);
+//			break;
+//		case 3:
+//			Calc(Mul);
+//			break;
+//		case 4:
+//			Calc(Div);
+//			break;
+//		case 0:
+//			printf("退出！\n");
+//			break;
+//		defalut:
+//			printf("选择错误\n");
+//			break;
+//		}
+//	} while (input);
+//}
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int(*p)[10] = &arr;//取出数组的地址
+//
+//	int(*pfArr[4])(int, int);//pfAff - 是一个函数指针的数组
+//	int(*(*ppfArr)[4])(int, int) = &pfArr;
+//	//ppfArr 是一个指向【函数指针数组】的指针
+//	//ppfArr 是一个指针，指针指向的数组有4个元素
+//	//指向的数组的每个元素的类型是一个函数指针 int(*)(int,int)
+//	return 0;
+//}
+
+void print(char* str)
 {
+	printf("hehe:%s", str);
+}
+
+void test(void(*p)(char*))
+{
+	printf("test\n");
+	p("bit");
 }
 
 int main()
 {
-	char*(*pf)(char*, const char*) = my_strcpy;
-	char* (*ppArr[4])(char*, const char*) = { my_strcpy };
+	test(print);
+	return 0;
 }
